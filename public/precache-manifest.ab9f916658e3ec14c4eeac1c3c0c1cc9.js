3e42716e96898724846e9a96835727c57e3f5534
Mahammad Zeynalov <magamed9r2.2011@gmail.com>
2020-06-24 16:26:16 +0400

    mvp

---
+++ public/precache-manifest.ab9f916658e3ec14c4eeac1c3c0c1cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39dee461909991f08a08ca7643e61da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44fa01944eec11c79ab1",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "21f42fed53f1e57b8a74",
+    "url": "/static/js/2.239605e3.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.239605e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44fa01944eec11c79ab1",
+    "url": "/static/js/main.6bc7de3c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
